56bf1b7aa12c6a39204c53ac282b9dd76f99eefb
ArianaMaciel <43325764+ArianaMaciel@users.noreply.github.com>
2019-11-30 15:07:24 -0300

    Add files via upload

---
+++ Controllers/APIController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    [Route("api")]
+    public class APIController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public APIController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Endereco/Enderecos")]
+        public IEnumerable<ConsultaCep> GetAll()
+        {
+            List<ConsultaCep> enderecos = _context.ConsultaCep.ToList();
+            return enderecos;
+        }
+
+
+    }
+}
